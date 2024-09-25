0962c8961368f1382c314e672c21d957f2bb02ef
Milca <milca.ekcauich@cbtis72.edu.mx>
2024-09-25 09:57:09 -0500

    Estilo corregido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  public title: string = 'Vivan Los Minions';
+  public title: string = 'Hola Mundo';
   public counter: number = 10;
 
   increaseBy (value:number) :void {
