54cc76d485db626bcbc13ac3a585a0b2fcdc8eb3
Milca <milca.ekcauich@cbtis72.edu.mx>
2024-10-16 09:43:45 -0500

    Dragon Ball Z

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
+
 import { AppComponent } from './app.component';
 
-import { CounterModule } from './counter/counter.module';
-import { HeroesModule } from './heroes/heroes.module';
-import { MainPageComponent } from './dbz/pages/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent,
-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
